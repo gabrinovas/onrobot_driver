8b10c3e30021c9c7be2fdb9823a83560613561b7
gabrinovas <gabriel.novas@aimen.es>
2025-10-01 17:11:08 +0200

    Third Approach - First package - v2

--- onrobot_driver/hardware/onrobot_gripper_plugin.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from onrobot_driver.hardware.onrobot_gripper_hardware_interface import OnRobotGripperHardwareInterface
-
-# Factory function for plugin system
-def get_hardware_interface():
-    return OnRobotGripperHardwareInterface()
